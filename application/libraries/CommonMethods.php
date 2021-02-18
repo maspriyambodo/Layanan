4da7b82876ab7b171e59b329767e493b1748636b
maspriyambodo <maspriyambodo@gmail.com>
2021-02-18 14:10:09 +0700

    add library multiupload

--- application/libraries/CommonMethods.php
+++ application/libraries/CommonMethods.php
@@ -33,22 +33,16 @@ class CommonMethods {
             $_FILES[$fieldName]['tmp_name'] = $files[$fieldName]['tmp_name'][$i];
             $_FILES[$fieldName]['error'] = $files[$fieldName]['error'][$i];
             $_FILES[$fieldName]['size'] = $files[$fieldName]['size'][$i];
-
             $this->CI->load->library('upload');
             $this->CI->upload->initialize($options);
-
             //upload the image
             if (!$this->CI->upload->do_upload($fieldName)) {
                 log_message('error', $this->upload->display_errors('<p>', '</p>'));
                 $result = false;
             } else {
-                $data = $this->upload->data();
-//            $picture = ['id' => '', 'nik' => $this->result[0]->nik, 'nopen' => $this->uri->segment(4), 'tgl_input' => date("Y-m-d h:i:s"), 'path' => "assets/images/lap_marketing/" . $data['file_name']];
-//            $this->M_Interaksi->Insertpict($picture);
-                $result = $data;
+                $result = $this->upload->data();
             }
         }
-
         return $result;
     }
 
