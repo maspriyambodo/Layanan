9b3d6e7eeef4eb2dd325f498a75cf875ce80def8
maspriyambodo <maspriyambodo@gmail.com>
2021-02-18 13:47:12 +0700

    add library multiupload

---
+++ application/libraries/CommonMethods.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * Product:        System of kementerian agama Republik Indonesia
+ * License Type:   Government
+ * Access Type:    Multi-User
+ * License:        https://maspriyambodo.com
+ * maspriyambodo@gmail.com
+ * 
+ * Thank you,
+ * maspriyambodo
+ */
+
+/**
+ * Description of do_upload_multiple_files
+ *
+ * @author centos
+ */
+class CommonMethods {
+
+    private $CI;
+
+    public function __construct() {
+        $this->CI = &get_instance();
+    }
+
+    public function do_upload_multiple_files($fieldName, $options) {
+
+        $response = [];
+        $files = $_FILES;
+        $cpt = count($_FILES[$fieldName]['name']);
+        for ($i = 0; $i < $cpt; $i++) {
+            $_FILES[$fieldName]['name'] = $files[$fieldName]['name'][$i];
+            $_FILES[$fieldName]['type'] = $files[$fieldName]['type'][$i];
+            $_FILES[$fieldName]['tmp_name'] = $files[$fieldName]['tmp_name'][$i];
+            $_FILES[$fieldName]['error'] = $files[$fieldName]['error'][$i];
+            $_FILES[$fieldName]['size'] = $files[$fieldName]['size'][$i];
+
+            $this->CI->load->library('upload');
+            $this->CI->upload->initialize($options);
+
+            //upload the image
+            if (!$this->CI->upload->do_upload($fieldName)) {
+                $response['erros'][] = $this->CI->upload->display_errors();
+            } else {
+                $response['result'][] = $this->CI->upload->data();
+            }
+        }
+
+        return $response;
+    }
+
+}
